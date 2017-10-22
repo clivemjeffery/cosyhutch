4e22abc7501deb3c598a10b2b9c3473ce93944ed
clivemjeffery <clive@rosedene.com>
2017-10-22 21:59:41 +0100

    More view tweaks, add time

--- cosyviz/cosyindex.py
+++ cosyviz/cosyindex.py
@@ -12,10 +12,15 @@ def svg_rect(x, y, w, h, radius=0, stroke_width=2, stroke='black', fill='white')
   svg = '<rect x="%i" y="%i" width="%i" height="%i" rx="%i" ry="%i" stroke-width="%i" stroke="%s" fill="%s" />' % (x, y, w, h, radius, radius, stroke_width, stroke, fill)
   return svg
 
+def svg_text_in_rect(caption, x, y, w, h, radius=0, stroke_width=2, stroke='black', fill='white'):
+  svg = svg_rect(x, y, w, h, radius, stroke_width, stroke, fill)
+  svg = '%s\n<text x="%i" y="%i" font-family="arial" font-size="32" fill="black" text-anchor="middle" alignment-baseline="central">%s</text>' % (svg, x+w/2, y+h/2, caption)
+  return svg
+
 def svg_temperature(x, y, caption, temperature, stroke='blue'):
   svg = svg_rect(x, y, 100, 50, 5, 5, stroke, 'black')
-  svg = '%s\n<text x="%i" y="%i" font-family="arial" font-size="18" fill="white">%s</text>' % (svg, x+5, y+15, caption)
-  svg = '%s\n<text x="%i" y="%i" font-family="arial" font-size="32" fill="white">%.1f</text>' % (svg, x+15, y+45, temperature)
+  svg = '%s\n<text x="%i" y="%i" font-family="arial" font-size="18" fill="%s">%s</text>' % (svg, x, y-5, stroke, caption)
+  svg = '%s\n<text x="%i" y="%i" font-family="arial" font-size="32" fill="white" text-anchor="middle" alignment-baseline="central">%.1f</text>' % (svg, x+50, y+25, temperature)
   return svg
   
 def main():
@@ -40,11 +45,14 @@ def main():
   of = open(args.index, 'w')
   print "    ...done."
 
+
+
   print "  Replacing tags in template..."
   for row in reader:
-    template = template.replace('$time', time.ctime(float(row['time'])))
-    template = template.replace('$outside', svg_temperature(50, 250, 'Outside', float(row['outside'])))
-    template = template.replace('$lavvy', svg_temperature(75, 100, 'Lavatory', float(row['lavvy']), 'cyan'))
+    timestr = time.strftime("%I:%M %p", time.gmtime(float(row['time'])))
+    template = template.replace('$time', svg_text_in_rect(timestr,615,5,180,50,5,5))
+    template = template.replace('$outside', svg_temperature(50, 250, 'Outside', float(row['outside']), 'cyan'))
+    template = template.replace('$lavvy', svg_temperature(75, 100, 'Lavatory', float(row['lavvy']), 'blue'))
     template = template.replace('$living', svg_temperature(260, 80, 'Lounge', float(row['living']), 'wheat'))
     template = template.replace('$boudoir', svg_temperature(625, 90, 'Boudoir', float(row['living']), 'green'))
   print "  ...done."
